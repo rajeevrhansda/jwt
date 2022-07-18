14b1dd5ce4eeefd9a04b0cf4ffe73e485ef5c725
Rajeev <rajeevrhansda@gmail.com>
2022-07-18 06:46:54 +0530

    dummy data add expressjson send response to client

--- api/index.js
+++ api/index.js
@@ -1,4 +1,25 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
-app.listen(5000,()=> console.log("Backend server is running"));
+const user = [
+    {
+        id: 1,
+        username: "John",
+        password: "John0908",
+        isAdmin: true
+    },
+    {
+        id: 1,
+        username: "John",
+        password: "John0908",
+        isAdmin: true
+    }
+]
+
+app.post("/api/login", (req,res)=>{
+    const {username , password} = req.body;
+    res.json("hey it works")
+});
+
+app.listen(5000, () => console.log("Backend server is running"));
