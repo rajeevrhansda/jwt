fcf47c27093e5382ec4012288200926506ff9bf5
Rajeev <rajeevrhansda@gmail.com>
2022-07-18 06:47:55 +0530

    find by username and password

--- api/index.js
+++ api/index.js
@@ -2,24 +2,32 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-const user = [
+const users = [
     {
         id: 1,
         username: "John",
-        password: "John0908",
+        password: "john0908",
         isAdmin: true
     },
     {
         id: 1,
-        username: "John",
-        password: "John0908",
+        username: "Jane",
+        password: "jane0908",
         isAdmin: true
     }
 ]
 
 app.post("/api/login", (req,res)=>{
     const {username , password} = req.body;
-    res.json("hey it works")
+    const user = users.find((u)=>{
+        return u.username === username && u.password === password;
+    });
+    if(user){
+        res.json(user);
+    }
+    else{
+        res.status(400).json("Username or password is incorrect")
+    }
 });
 
 app.listen(5000, () => console.log("Backend server is running"));
